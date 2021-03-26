dd5f489933c86d8432da139c083522d24d13db92
svenleonhard <sven.leonhard@7lofficial.de>
2021-03-26 19:24:06 +0100

    [work/redirection-update] remove unused import

--- src/main/webapp/app/qr-codes/update/update-modal.component.ts
+++ src/main/webapp/app/qr-codes/update/update-modal.component.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular
 import { FormBuilder, NgForm } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
-import { QrCodeService } from 'app/entities/qr-code/qr-code.service';
 import { IQrCode } from 'app/shared/model/qr-code.model';
 import { RedirectionService } from 'app/entities/redirection/redirection.service';
 
