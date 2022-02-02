0e7401bfe59644632cc9736fefa46f5af3dd1ae3
svenleonhard <mail@svenleonhard.de>
2022-02-02 22:57:13 +0100

    user browser language as default

--- src/main/webapp/app/shared/language/translation.module.ts
+++ src/main/webapp/app/shared/language/translation.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateService, TranslateLoader, MissingTranslationHandler } from '@ngx-translate/core';
 import { translatePartialLoader, missingTranslationHandler } from 'app/config/translation.config';
@@ -21,9 +21,15 @@ import { SessionStorageService } from 'ngx-webstorage';
 })
 export class TranslationModule {
   constructor(private translateService: TranslateService, sessionStorageService: SessionStorageService) {
-    translateService.setDefaultLang('en');
+    let browserLang = translateService.getBrowserLang();
+
+    if (!browserLang) {
+      browserLang = 'en';
+    }
+
+    translateService.setDefaultLang(browserLang);
     // if user have changed language and navigates away from the application and back to the application then use previously choosed language
-    const langKey = sessionStorageService.retrieve('locale') ?? 'en';
+    const langKey = sessionStorageService.retrieve('locale') ?? browserLang;
     translateService.use(langKey);
   }
 }
